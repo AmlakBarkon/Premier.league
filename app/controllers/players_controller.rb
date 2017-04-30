d98a808f1effaf7b1fb31f3449a9b623776c93aa
amlak befekadu <amlakgirma@yahoo.com>
2017-04-30 16:47:24 -0400

    done with CRUD OF TEAM. HALF DONE WITH PLAYER

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,5 +2,12 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
   end
-  
+  def new
+    @player = Player.find(params[:player_id])
+    @team = Team.new
+  end
+  def edit
+    @team = Team.find(params[:team_id])
+    @playeer = Player.find(params[:id])
+  end
 end
